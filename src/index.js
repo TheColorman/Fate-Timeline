a69b55e153daf6a1b65833d9389a36c3c077318f
Alexander Thoren <thoren.alex@gmail.com>
2021-11-20 01:17:58 +0100

    :heavy_plus_sign: Added Bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Flow from "./flow"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // ========================================
