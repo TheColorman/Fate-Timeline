265804112badfe68bb07987bf3ee0974ea470b5e
Alexander Thoren <thoren.alex@gmail.com>
2021-11-20 01:19:50 +0100

    :sparkles: Added series + data points

--- src/images/images.js
+++ src/images/images.js
@@ -2,10 +2,16 @@ import i_Zero from './Zero.jpg';
 import i_SN from './SN.jpg';
 import i_UBW from './UBW.jpg';
 import i_HF from './HF.jpg';
+import i_TMEF from './TMEF.jpg';
+import i_LEMCF from './LEMCF.jpg';
+import i_KL from './KL.jpg';
 
 export {
     i_Zero,
     i_SN,
     i_UBW,
     i_HF,
+    i_TMEF,
+    i_LEMCF,
+    i_KL,
 }
